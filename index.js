2633ada5b5f749b01400610e28fc7705b707708e
Michael Mason <m.masonn3@gmail.com>
2023-06-12 16:30:04 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 =(31)
+let num2 =(2)
+let multiply = (62)
 
-// Write your code here
+let random = 6
+
+let num3 =(16)
+let num4 =(4)
+let divide = (4)
+let mod= 4
+
+Math.max(15,16,17,18,19,20)
+let max = 20 
